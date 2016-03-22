f0d007ccc90017ca161bcb4da90c2b745bf31283
Ville Toikkanen <vtoikkanen@hotmail.fi>
2016-03-22 16:49:52 +0200

    Updated the code from variable based to array based system

--- vtjs.js
+++ vtjs.js
@@ -1,49 +1,51 @@
 $(document).ready(function() {
 	// Open object
-	$(open_activator).click(function() {
-		if(open_method == 'appear') {
-			$(open_id).css('display', 'block');
-		}else if(open_method == 'fade') {
-			$(open_id).fadeIn(open_animdur);
+	var open_activator_array = open_activator.join(", "); // Make array work as selector in click-function
+	$(open_activator_array).click(function() {
+		var open_clicked_id = '#' + this.id; // Store clicked objects id in variable
+		var clicked_open_activator = $.inArray(open_clicked_id, open_activator); // Get clicked object's position in array
+		if(clicked_open_activator == -1) { // Check if object was not found (will return -1 instead of 0 or more)
+			open_clicked_id = '.' + this.className; // Store clicked objects class in variable
+			clicked_open_activator = $.inArray(open_clicked_id, open_activator); // Now check if object with class instead of id was found in array
+			var target_open_id = open_id[clicked_open_activator]; // Store target object class to variable
+			
+		}else { // ID was found, no need to check if class was found
+			var target_open_id = open_id[clicked_open_activator]; // Store target objects id to variable
+			var target_open_method = open_method[clicked_open_activator]; // Store target objects opening method to variable
+			var target_open_animdur = open_animdur[clicked_open_activator]; // Store target objects animation duration to variable
+			if(target_open_method == 'appear') { // Check if opening method is set to appear
+				$(target_open_id).css('display', 'block'); // Change objects display to block
+			}else if(target_open_method == 'fade') { // Check if opening method is set to fade
+				$(target_open_id).fadeIn(target_open_animdur); // Fade object in by chosen duration
+			}
 		}
 	});
 	
-	// Close object 
-	$(close_activator).click(function() {
-		if(close_method == 'disappear') {
-			$(close_id).css('display', 'none');
-		}else if(close_method == 'fade') {
-			$(close_id).fadeIn(close_animdur);
+	// Close object
+	var close_activator_array = close_activator.join(", ");
+	$(close_activator_array).click(function() {
+		var close_clicked_id = '#' + this.id;
+		var clicked_close_activator = $.inArray(close_clicked_id, close_activator);
+		if(clicked_close_activator <= -1) {
+			alert('works');
+			close_clicked_id = '.' + thid.id;
+			var target_close_id = close_id[clicked_close_activator];
+			var target_close_method = close_method[clicked_close_activator];
+			var target_close_animdur = close_animdur[clicked_close_activator];
+			if(target_close_method == 'disappear') {
+				$(target_close_id).css('display', 'none');
+			}else if(target_close_method == 'fade') {
+				$(target_close_id).fadeOut(target_close_animdur);
+			}
+		}else {
+			var target_close_id = close_id[clicked_close_activator];
+			var target_close_method = close_method[clicked_close_activator];
+			var target_close_animdur = close_animdur[clicked_close_activator];
+			if(target_close_method == 'disappear') {
+				$(target_close_id).css('display', 'none');
+			}else if(target_close_method == 'fade') {
+				$(target_close_id).fadeOut(target_close_animdur);
+			}
 		}
-	});
-	
-	// Open and close
-	$(openclose_activator).click(function() {
-		if($(openclose_id).css('display') == 'none') {
-			$(openclose_id).css('display', 'block');
-		}else if($(openclose_id).css('display') == 'block') {
-			$(openclose_id).css('display', 'none');
-		}
-	});
-	
-	// Scroll to one position
-	$(scrollto_activator).click(function() {
-		if(scrollto_method == 'animation') {
-			$('html, body').animate({
-				scrollTop: $(scrollto_target).offset().top
-			}, 300);
-		}else if(scrollto_method == 'default') {
-			$('html, body').animate({
-				scrollTop: $(scrollto_target).offset().top
-			}, 0);
-		}
-	});
-	
-	// Scroll to with multiple elements
-	$(scrollto_multiple_activator).click(function() {
-		var object_id = scrollto_multiple_target + '.' + this.id;
-		$('html, body').animate({
-				scrollTop: $(object_id).offset().top
-		}, 300);
 	});
 });
